da2da3c3e8c87749220a82892d5b4707cc7e0ca5
Wajahat Khan <wajahat.khan@technifiedlabs.com>
2025-07-05 00:35:09 +0500

    fav icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <!-- Scripts -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="icon" href="https://vortexdigitallabs.com/assets/images/favicon.png" type="image/x-icon" />
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <style>
