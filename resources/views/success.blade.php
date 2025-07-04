b8d628ce49c9a84b067adbeea400b5334de64ea3
Wajahat Khan <wajahat.khan@technifiedlabs.com>
2025-07-05 02:07:47 +0500

    fav icon

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -8,7 +8,7 @@
         <div class="mb-4">
             <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="#28a745" class="bi bi-check-circle" viewBox="0 0 16 16">
               <path d="M15.854 7.146a.5.5 0 0 1 0 .708l-8 8a.5.5 0 0 1-.708 0l-4-4a.5.5 0 1 1 .708-.708L7.5 14.293l7.646-7.647a.5.5 0 0 1 .708 0z"/>
-              <path d="M1 8a7 7 0 1 1 14 0A7 7 0 0 1 1 8z"/>
+              <!-- <path d="M1 8a7 7 0 1 1 14 0A7 7 0 0 1 1 8z"/> -->
             </svg>
         </div>
         <h2 class="fw-bold mb-3">Payment Successful!</h2>
