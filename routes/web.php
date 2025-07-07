806aba3afe9d64c95f24351688fa9c5c3303fc37
Wajahat Khan <74681804+wajahatk795@users.noreply.github.com>
2025-07-08 01:45:30 +0500

    Payment link integration

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@ Route::middleware(['auth'])->group(function () {
 
 Route::get('/pay/{id}', [StripePaymentController::class, 'showCustomPaymentPage'])->name('custom-pay');
 Route::get('/success', [StripePaymentController::class, 'success'])->name('success');
+Route::get('/cancel', function () {
+    return view('cancel');
+})->name('cancel');
 
 require __DIR__.'/auth.php';
