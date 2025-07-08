a5e250eed05dad3a47abb76dc405f4dcd90d9a3d
Wajahat Khan <74681804+wajahatk795@users.noreply.github.com>
2025-07-09 00:55:23 +0500

    Payment status update and dashboard page creation

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\PaymentRequest;
-use App\Mail;
+use Illuminate\Support\Facades\Mail;
 
 class StripePaymentController extends Controller
 {
